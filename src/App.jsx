980154dce0114bab134f37f1cdbc9c08995b186c
Ricardo Zelante <r.a.zelante@gmail.com>
2026-01-23 15:14:08 -0600

    fixed import typo of NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import "./app.scss"
+import "./app.scss";
 import Contact from "./components/contact/Contact";
 import Cursor from "./components/cursor/Cursor";
 import Hero from "./components/hero/Hero";
-import Navbar from "./components/navbar/Navbar";
+import Navbar from "./components/navbar/NavBar";
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
 import Skills from "./components/services/Skills";
-
 
 const App = () => {
   return (
@@ -19,14 +18,23 @@ const App = () => {
         <Hero />
       </section>
 
-      <section id="Services"><Parallax type="services"/></section>
-      <section> <Skills /> </section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
+      <section>
+        {" "}
+        <Skills />{" "}
+      </section>
 
-      <section id="Portfolio"><Parallax type="portfolio"/></section>
-      
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
+
       <Portfolio />
-      
-      <section id="Contact"><Contact /></section>
+
+      <section id="Contact">
+        <Contact />
+      </section>
     </div>
   );
 };
